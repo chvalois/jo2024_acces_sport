19abeb566a13da0edbe7aa692394814343fc7df1
chvalois <chvalois@yahoo.fr>
2024-08-06 16:59:25 +0200

    Ajout d'une viz par défaut sur la page France

--- src/apps/france.py
+++ src/apps/france.py
@@ -9,6 +9,14 @@ from src.functions.map_functions import display_france_map
 
 def france():
 
+    # Initialize the session state if it does not already exist
+    if 'form_submitted' not in st.session_state:
+        st.session_state.form_submitted = False
+
+    # Function to handle form submission
+    def submit_form():
+        st.session_state.form_submitted = True
+
     es_sports, fed_sports = get_mappings()
     fed_sports_list = fed_sports[fed_sports['sport'] != 'Divers']['federation'].to_list()
     fed_sports_list = sorted(fed_sports_list)
@@ -26,16 +34,22 @@ def france():
     with st.form('france'):
         col1, col2 = st.columns(2)
         with col1:
-            fed = st.selectbox("Choisir une fédération", fed_sports_list)
+            fed = st.selectbox("Choisir une fédération", fed_sports_list, index = 11)
 
         with col2:
             stat = st.selectbox("Choisir une statistique", stat_options)
         qpv = st.checkbox("Afficher les statistiques dans les QPV seulement", help = "QPV = Quartiers Prioritaires de la Politique de la Ville")
-        submitted = st.form_submit_button("Valider")
+        submit_button = st.form_submit_button(label='Valider', on_click=submit_form)
 
-    if submitted:
+    # Display a container if the form is not filled
+    if not st.session_state.form_submitted:
+        with st.container():
+            st.subheader(f"Exemple | FF de Basketball | Pourcentage de femmes licenciées en France")    
+            m = display_france_map("FF de Basketball", "Pourcentage de femmes licenciées", qpv=False)
+            folium_static(m, height = 750)
+
+    if st.session_state.form_submitted:
         with st.spinner('Veuillez patienter ...'):
-
             st.subheader(f"{fed} | {stat} en France")           
             if qpv:
                 st.write("Statistiques des quartiers prioritaires uniquement")
@@ -43,3 +57,4 @@ def france():
 
             m = display_france_map(fed, stat, qpv)
             folium_static(m, height = 750)
+
